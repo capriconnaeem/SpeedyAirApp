c6e561094f83b02a6907c4a57eb96f3c9399f33b
Naeem Akhtar <capriconnaeem@gmail.com>
2024-11-07 19:19:03 -0500

    added

--- SpeedyAir.Data/obj/Debug/net8.0/SpeedyAir.Data.AssemblyInfo.cs
+++ SpeedyAir.Data/obj/Debug/net8.0/SpeedyAir.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpeedyAir.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41ad4f25cd34cfb72929f5644631a66f9ef5a7d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpeedyAir.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpeedyAir.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SpeedyAirConsoleApp/obj/Debug/net8.0/SpeedyAir.ConsoleApp.AssemblyInfo.cs
+++ SpeedyAirConsoleApp/obj/Debug/net8.0/SpeedyAir.ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpeedyAir.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41ad4f25cd34cfb72929f5644631a66f9ef5a7d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpeedyAir.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpeedyAir.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
